ad0eba05899051915a44c41e4437cdca92e83d0a
unknown <minpyaesone82>
2022-05-10 21:24:14 +0630

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,9 @@
         <header >welcome </header>
 
         {{ date('d M Y h:i A') }}
+        kasdhfasdfasdfbasd
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, minima ad est velit impedit debitis aliquam iusto ab eos beatae non recusandae in fugit tempora assumenda ratione minus repellat dolore!
     @endguest
+
     
 @endsection
